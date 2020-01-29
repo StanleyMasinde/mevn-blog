40fbe2df467ae80acad4fae9ccdf44158f734e0b
Stanley Masinde <stanleyloren@gmail.com>
2020-01-29 13:53:05 +0300

    version two

--- app.js
+++ app.js
@@ -2,23 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var csrf = require('csurf')
-var bodyParser = require('body-parser')
-require('./database/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
-var authRouter = require('./routes/auth')
 
 var app = express();
 
-// setup route middlewares
-var csrfProtection = csrf({ cookie: true })
-var parseForm = bodyParser.urlencoded({ extended: false })
-
-app.engine('html', require('ejs').renderFile);
-app.set('views', __dirname + '/public');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,11 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
-app.use('/auth', authRouter)
-app.get('/*/',csrfProtection, function(req, res, next) {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.render('index.html', {token: req.csrfToken()})
-})
 
 module.exports = app;
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('src/app.js', 'public/javascripts')
-   .sass('src/app.scss', 'public/stylesheets')
-   .setPublicPath('public');
